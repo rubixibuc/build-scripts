dd1b86fe6702b24643c1fddfdf0c19a243d039e1
Jason Rubenstein <rubenstein.jason.i@gmail.com>
2021-12-28 10:28:13 -0500

    return error code when prettier fails

--- commands/prettier.js
+++ commands/prettier.js
@@ -2,13 +2,22 @@ module.exports = async (globs, options) => {
   const spawn = require("cross-spawn");
 
   if (options.fix) {
-    spawn.sync("npx", ["--no-install", "prettier", "--write", ...globs], {
-      stdio: "inherit",
-    });
+    const result = spawn.sync(
+      "npx",
+      ["--no-install", "prettier", "--write", ...globs],
+      {
+        stdio: "inherit",
+      }
+    );
+
+    process.exitCode = result.status;
+
     return;
   }
 
-  spawn.sync("npx", ["--no-install", "prettier", ...globs], {
+  const result = spawn.sync("npx", ["--no-install", "prettier", ...globs], {
     stdio: "inherit",
   });
+
+  process.exitCode = result.status;
 };
