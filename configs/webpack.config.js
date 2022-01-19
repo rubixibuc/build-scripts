d1dab99f07faf9f4ab270a5dab3c72347d56587d
Jason Rubenstein <rubenstein.jason.i@gmail.com>
2022-01-18 22:09:54 -0500

    remove images folder since there is only one main image

--- configs/webpack.config.js
+++ configs/webpack.config.js
@@ -11,7 +11,7 @@ module.exports = ({
   background,
   exposes,
   links = [],
-  logo = require.resolve("../images/logo.png"),
+  logo = require.resolve("../logo.png"),
   metas,
   port,
   remotes,
